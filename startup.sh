84bc10a3295673924dcd8ffe778a1da1f198481b
DiegoPerez1441 <52579214+DiegoPerez1441@users.noreply.github.com>
2020-10-27 16:54:13 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,24 +1,12 @@
-wget https://i.imgflip.com/1uv9gt.jpg && mv 1uv9gt.jpg PressMe.jpg ; wget https://i.ytimg.com/vi/IyqQOtGHln8/maxresdefault.jpg && mv maxresdefault.jpg Minecraft.jpg
+mkdir ~/DRR
+mkdir ~/ProjectBackup
 
-sleep 5
-clear
-sleep 1
+cwd=$(pwd)
+cd $cwd ; cd .. ; rwd=$(pwd)
+mv $cwd/.replit $rwd
 
-echo "\033[0;31m Please Open the new Files \033[0;0m"
-sleep 3
-echo "\033[0;31m The Program WON'T RUN UNLESS YOU OPEN THE NEW FILES FIRST \033[0;0m"
-
-sleep 10
-rm Minecraft.jpg
-sleep 1
-rm PressMe.jpg
-
-sleep 3
-rm .replit
-sleep 1
+cd $cwd
+chmod +x DRRS.sh
+mv DRRS.sh ~/DRR
 rm startup.sh
-
-clear
-
-javac -classpath .:/run_dir/junit-4.12.jar:target/dependency/* -d . Main.java
-java -classpath .:/run_dir/junit-4.12.jar:target/dependency/* Main
+rm -rf $rwd/Repl.it_DissapearingRickRoll
