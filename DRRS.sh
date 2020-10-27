abc13611c5e9cfcd2266c7b6783ab7bf5a2ea008
DiegoPerez1441 <52579214+DiegoPerez1441@users.noreply.github.com>
2020-10-27 16:54:32 -0500

    Create DRRS.sh

---
+++ DRRS.sh
@@ -0,0 +1,20 @@
+cd $rwd
+echo pwd
+#wget https://i.imgflip.com/1uv9gt.jpg && mv 1uv9gt.jpg PressMe.jpg
+
+#sleep 5
+#clear
+#sleep 1
+
+#echo "\033[0;31m You've Been Rick Rolled! \033[0;0m"
+#sleep 3
+
+#sleep 3
+#rm .replit
+#sleep 1
+#rm startup.sh
+
+#clear
+
+#javac -classpath .:/run_dir/junit-4.12.jar:target/dependency/* -d . Main.java
+#java -classpath .:/run_dir/junit-4.12.jar:target/dependency/* Main
