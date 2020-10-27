cef905f99292a1703454d3867ea6f49364bc2745
DiegoPerez1441 <52579214+DiegoPerez1441@users.noreply.github.com>
2020-10-27 17:21:12 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
 mkdir ~/DRR
+mkdir ~/DRR/tmp
 mkdir ~/ProjectBackup
 
 cwd=$(pwd)
 cd $cwd ; cd .. ; rwd=$(pwd)
+touch ~/DRR/tmp/rwd.txt
+echo $rwd > ~/DRR/tmp/rwd.txt
 mv $cwd/.replit $rwd
 
 cd $cwd
